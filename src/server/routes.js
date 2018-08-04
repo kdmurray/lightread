8fef5c7e135d101d02afea2ac6d8cc03745906ac
Keith Murray <kdmurray@gmail.com>
2018-08-03 20:47:55 -0700

    Added new routes. Started some to-do docs.

--- src/server/routes.js
+++ src/server/routes.js
@@ -6,7 +6,25 @@ var router = PromiseRouter();
 //
 // Create a route for the homepage
 router.get('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200);
     res.send('home!');
+});
+
+//
+// Create a route for the login page
+router.get('/login', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200);
+    res.send('Please enter your username and password...');
+});
+
+//
+// Create a route for the user registration page
+router.get('/register', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200);
+    res.send('Thanks for your interest! Please register for your account below...');
 });
 
 //
